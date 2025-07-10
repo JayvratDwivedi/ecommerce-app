f3dbfbc29c5e70a3bc50e33a57056bec3092b27a
Jayvrat Dwivedi <jayvratdwivedi202@gmail.com>
2025-07-10 15:55:55 +0530

    feat(security): add JWTRequestFilter to security chain and configure public endpoints

--- backend/src/main/java/com/nextphase/backend/api/security/WebSecurityConfig.java
+++ backend/src/main/java/com/nextphase/backend/api/security/WebSecurityConfig.java
@@ -5,15 +5,25 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
 
 @Configuration
 public class WebSecurityConfig {
+    private JWTRequestFilter jwtRequestFilter;
+
+    public WebSecurityConfig(JWTRequestFilter jwtRequestFilter) {
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests((authorizationManagerRequestMatcherRegistry)->authorizationManagerRequestMatcherRegistry.anyRequest().permitAll());
+                .addFilterBefore(jwtRequestFilter, AuthorizationFilter.class)
+                .authorizeHttpRequests((authorizationManagerRequestMatcherRegistry)->authorizationManagerRequestMatcherRegistry
+                        .requestMatchers("/product", "/auth/register", "/auth/login").permitAll()
+                        .anyRequest().authenticated());
         return httpSecurity.build();
     }
 }
