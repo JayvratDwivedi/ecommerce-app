10273f9a3c9d4909f595e54555e8eb97961ad08f
Jayvrat Dwivedi <jayvratdwivedi202@gmail.com>
2025-07-16 16:11:43 +0530

    fix: major security bug fix in jwt

--- backend/src/main/java/com/nextphase/backend/service/JWTService.java
+++ backend/src/main/java/com/nextphase/backend/service/JWTService.java
@@ -2,6 +2,7 @@ package com.nextphase.backend.service;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.nextphase.backend.model.LocalUser;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
@@ -33,14 +34,15 @@ public class JWTService {
                 .withIssuer(issuer)
                 .sign(algorithm);
     }
-    public String getUsername(String token) {
-        return JWT.decode(token).getClaim(USERNAME_KEY).asString();
-    }
     public String generateVerificationJWT(LocalUser localUser) {
         return JWT.create()
                 .withClaim(EMAIL_KEY, localUser.getEmail())
                 .withExpiresAt(new Date(System.currentTimeMillis() + (1000 * expiryInSeconds)))
                 .withIssuer(issuer)
                 .sign(algorithm);
+    }
+    public String getUsername(String token) {
+        DecodedJWT jwt = JWT.require(algorithm).withIssuer(issuer).build().verify(token);
+        return jwt.getClaim(USERNAME_KEY).asString();
     }
 }
--- backend/src/test/java/com/nextphase/backend/service/JWTServiceTest.java
+++ backend/src/test/java/com/nextphase/backend/service/JWTServiceTest.java
@@ -1,10 +1,15 @@
 package com.nextphase.backend.service;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.MissingClaimException;
+import com.auth0.jwt.exceptions.SignatureVerificationException;
 import com.nextphase.backend.model.LocalUser;
 import com.nextphase.backend.model.dao.LocalUserDAO;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -16,6 +21,9 @@ public class JWTServiceTest {
 
     @Autowired
     private LocalUserDAO localUserDAO;
+
+    @Value("${jwt.algorithm.key}")
+    private String algorithmKey;
 
     @Test
     public void testVerificationTokenNotUsableForLogin() {
@@ -30,4 +38,24 @@ public class JWTServiceTest {
         String token = jwtService.generateJWT(user);
         Assertions.assertEquals(user.getUsername(), jwtService.getUsername(token), "Token for auth should contain users username.");
     }
+
+    @Test
+    public void testJWTNotGeneratedByUs() {
+        String token =
+                JWT.create().withClaim("USERNAME", "UserA").sign(Algorithm.HMAC256(
+                        "NotTheRealSecret"));
+        Assertions.assertThrows(SignatureVerificationException.class,
+                () -> jwtService.getUsername(token));
+    }
+
+    @Test
+    public void testJWTCorrectlySignedNoIssuer() {
+        String token =
+                JWT.create().withClaim("USERNAME", "UserA")
+                        .sign(Algorithm.HMAC256(algorithmKey));
+        Assertions.assertThrows(MissingClaimException.class,
+                () -> jwtService.getUsername(token));
+    }
+
+
 }
