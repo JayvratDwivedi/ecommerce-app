5d9fae16241a18141d386adc3d68b66399470274
Jayvrat Dwivedi <jayvratdwivedi202@gmail.com>
2025-07-16 10:52:32 +0530

    add OrderControllerTest.java

---
+++ backend/src/test/java/com/nextphase/backend/api/controller/order/OrderControllerTest.java
@@ -0,0 +1,9 @@
+package com.nextphase.backend.api.controller.order;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class OrderControllerTest {
+}
