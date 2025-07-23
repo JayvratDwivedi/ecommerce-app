cce4c9245978fcc1e00f3c8c4bd68f6d4ed0c5ab
Jayvrat Dwivedi <jayvratdwivedi202@gmail.com>
2025-07-23 11:27:24 +0530

    add functions to add and edit addresses

--- backend/src/main/java/com/nextphase/backend/api/controller/user/UserController.java
+++ backend/src/main/java/com/nextphase/backend/api/controller/user/UserController.java
@@ -6,13 +6,11 @@ import com.nextphase.backend.model.dao.AddressDAO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
@@ -25,22 +23,51 @@ public class UserController {
     }
 
     @GetMapping("/{userId}/address")
-    public ResponseEntity<List<Address>> getAddress(@PathVariable Long userId){
+    public ResponseEntity<List<Address>> getAddress(
+            @AuthenticationPrincipal LocalUser user, @PathVariable Long userId) {
+        if (userHasPermission(user, userId)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
         return ResponseEntity.ok(addressDAO.findByUser_Id(userId));
     }
 
-//    @GetMapping("/{userId}/address")
-//    public ResponseEntity<List<Address>> getAddress(
-//            @AuthenticationPrincipal LocalUser user, @PathVariable Long userId) {
-//        if (!userHasPermission(user, userId)) {
-//            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
-//        }
-//        return ResponseEntity.ok(addressDAO.findByUser_Id(userId));
-//    }
-//
-//    private boolean userHasPermission(LocalUser user, Long id) {
-//        return Objects.equals(user.getId(), id);
-//    }
+    @PutMapping("/{userId}/address")
+    public ResponseEntity<Address> putAddress(
+            @AuthenticationPrincipal LocalUser user, @PathVariable Long userId,
+            @RequestBody Address address) {
+        if (userHasPermission(user, userId)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+        address.setId(null);
+        LocalUser refUser = new LocalUser();
+        refUser.setId(userId);
+        address.setUser(refUser);
+        return ResponseEntity.ok(addressDAO.save(address));
+    }
+
+    @PatchMapping("/{userId}/address/{addressId}")
+    public ResponseEntity<Address> patchAddress(
+            @AuthenticationPrincipal LocalUser user, @PathVariable Long userId,
+            @PathVariable Long addressId, @RequestBody Address address) {
+        if (userHasPermission(user, userId)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+        if (Objects.equals(address.getId(), addressId)) {
+            Optional<Address> opOriginalAddress = addressDAO.findById(addressId);
+            if (opOriginalAddress.isPresent()) {
+                LocalUser originalUser = opOriginalAddress.get().getUser();
+                if (Objects.equals(originalUser.getId(), userId)) {
+                    address.setUser(originalUser);
+                    return ResponseEntity.ok(addressDAO.save(address));
+                }
+            }
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+    private boolean userHasPermission(LocalUser user, Long id) {
+        return !Objects.equals(user.getId(), id);
+    }
 
 
 
